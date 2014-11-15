de73d015547c1947a3676ae6e1753c4c20cd76d3
Ziying <ziying95@hotmail.com>
2014-11-15 16:19:32 -0500

    work

---
+++ public/js/app/views/View.js
@@ -0,0 +1,50 @@
+// View.js
+// -------
+define(["jquery", "backbone", "models/Model", "text!templates/heading.html", "views/auth"],
+
+    function($, Backbone, Model, template, AuthView){
+
+        var View = Backbone.View.extend({
+
+            // The DOM Element associated with this view
+            el: ".example",
+
+            // View constructor
+            initialize: function() {
+
+                // Calls the view's render method
+                this.render();
+
+            },
+
+            // View Event Handlers
+            events: {
+                "click .test-button": "testFun"
+            },
+            testFun: function() {
+                alert("it works");
+                var authView= new AuthView();
+
+            },
+            // Renders the view's template to the UI
+            render: function() {
+
+                // Setting the view's template property using the Underscore template method
+                this.template = _.template(template, {});
+
+                // Dynamically updates the UI with the view's template
+                this.$el.html(this.template);
+
+                // Maintains chainability
+                return this;
+
+            }
+
+        });
+
+        // Returns the View class
+        return View;
+
+    }
+
+);
